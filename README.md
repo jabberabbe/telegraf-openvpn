b1a98dcde12a09dc9d5e9968db166ac9c1be1123
Tito Sacchi <tito.sakki@gmail.com>
2021-08-03 12:04:31 +0200

    Parse OpenVPN data and write on stdout

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # telegraf-openvpn
 
+[![GitHub CI](https://github.com/jabberabbe/telegraf-openvpn/workflows/CI/badge.svg)](https://github.com/jabberabbe/telegraf-openvpn/actions)
+[![MIT license](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE)
+
 This script emits OpenVPN server statistics such as connected clients and total
 sent/recvd bytes in InfluxDB data format. It is meant to be used with the
 `execd` Telegraf plugin. It queries the server process via the management
 socket.
-
-[![GitHub CI](https://github.com/jabberabbe/telegraf-openvpn/workflows/CI/badge.svg)](https://github.com/jabberabbe/telegraf-openvpn/actions)
-[![MIT license](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE)
 
--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,106 @@
+{-# LANGUAGE LambdaCase          #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE RecordWildCards     #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ViewPatterns        #-}
+
 module Main (main) where
 
+import           Conduit                  hiding (connect)
+import           Control.Concurrent.Async
+import           Control.Exception
+import           Control.Monad
+import           Control.Monad.State
+import           Data.Conduit.Network
+import           Data.CSV.Conduit
+import           Data.Time.Clock
+import           Data.Time.Clock.POSIX
+import           Debug.Trace
+import           Network.Socket
+import           System.Environment
+import           System.Exit
+
 main :: IO ()
-main = putStrLn ("Hello, world!" :: String)
+main = do
+    socketPath <- getSocketPath
+    bracket (socket AF_UNIX Stream defaultProtocol) close $ \sock -> do
+        connect sock (SockAddrUnix socketPath)
+        race_
+            (flip execStateT undefined $ runConduit $ requestStatus sock)
+            (flip execStateT undefined $ runConduit $ processStatus sock)
+  where
+    getSocketPath = do
+        args <- getArgs
+        when (length args /= 1) $ do
+            putStrLn "Usage: telegraf-openvpn /path/to/openvpn-manage.sock"
+            exitWith $ ExitFailure 1
+        pure $ head args
+
+-- Telegraf sends "\n" on stdin every tick
+requestStatus :: Socket -> ConduitT () Void (StateT UTCTime IO) ()
+requestStatus sock = stdinC .| mapC (const "status 2\n") .| sinkSocket sock
+
+processStatus :: Socket -> ConduitT () Void (StateT UTCTime IO) ()
+processStatus sock = sourceSocket sock .| intoCSV defCSVSettings .|
+    mapM_C (\case {
+    ; [ "TIME", _, parseTimestamp -> time ] ->
+      put time
+
+    ; [ "CLIENT_LIST", commonName, realAddress, notEmpty -> virtualIp4
+      , notEmpty -> virtualIp6, read -> bytesRecv, read -> bytesSent, _, parseTimestamp -> connectedSince
+      , notUndef -> username, read -> clientID, read -> peerID, cipher ] ->
+          lift . putStrLn . influxLineFormat ClientStatus{..} =<< get
+
+    ; _ -> pure ()
+    })
+  where
+    parseTimestamp = posixSecondsToUTCTime . fromIntegral . (read :: String -> Integer)
+
+    notEmpty "" = Nothing
+    notEmpty x  = Just x
+
+    notUndef "UNDEF" = Nothing
+    notUndef x       = Just x
+
+data ClientStatus = ClientStatus
+    { commonName     :: String
+    , realAddress    :: String
+    , virtualIp4     :: Maybe String
+    , virtualIp6     :: Maybe String
+    , bytesRecv      :: Word
+    , bytesSent      :: Word
+    , connectedSince :: UTCTime
+    , username       :: Maybe String
+    , clientID       :: Word
+    , peerID         :: Word
+    , cipher         :: String }
+    deriving (Show)
+
+influxLineFormat :: ClientStatus -> UTCTime -> String
+influxLineFormat p@ClientStatus{..} time =
+    "openvpn," ++
+
+    "commonName=" ++ escape commonName ++ "," ++
+    "realAddress=" ++ escape realAddress ++ "," ++
+    maybe "" (\ip -> "virtualIp4=" ++ escape ip ++ ",") virtualIp4 ++
+    maybe "" (\ip -> "virtualIp6=" ++ escape ip ++ ",") virtualIp6 ++
+    maybe "" (\uname -> "userName=" ++ escape uname ++ ",") username ++
+    "cipher=" ++ escape cipher ++ " " ++
+
+    "bytesRecv=" ++ show bytesRecv ++ "u," ++
+    "bytesSent=" ++ show bytesSent ++ "u," ++
+    "connectedSince=" ++ (timeToInfluxFormat connectedSince) ++ "u," ++
+    "clientId=" ++ show clientID ++ "u," ++
+    "peerId=" ++ show peerID ++ "u " ++
+
+    (timeToInfluxFormat time)
+  where
+    escape xs = "\"" ++ concatMap f xs ++ "\""
+
+    f '\\' = "\\\\"
+    f '"'  = "\\\""
+    f x    = [x]
+
+    timeToInfluxFormat = show . floor . (1e9 *)
+      . nominalDiffTimeToSeconds . utcTimeToPOSIXSeconds
 
